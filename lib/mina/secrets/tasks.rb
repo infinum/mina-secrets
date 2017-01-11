3bb1dceb63ad80161d235243094e03dac33ad099
Stjepan Hadjic <d4be4st@gmail.com>
2017-01-11 12:43:57 +0100

    add Bundle exec to secrets

--- lib/mina/secrets/tasks.rb
+++ lib/mina/secrets/tasks.rb
@@ -15,6 +15,6 @@ namespace :secrets do
   desc 'Secrets pull'
   task pull: :environment do
     comment 'Secrets pull'
-    command "secrets pull -e #{fetch(:rails_env)} -d #{fetch(:shared_path)} -y"
+    command "bundle exec secrets pull -e #{fetch(:rails_env)} -d #{fetch(:shared_path)} -y"
   end
 end
